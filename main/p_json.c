2e9997546d08d2f58baf9df757e439b15ab6e801
Subhajit Sahu <wolfram77@gmail.com>
2019-06-29 21:23:13 +0530

    remove logs in json

--- main/p_json.c
+++ main/p_json.c
@@ -4,13 +4,9 @@
 
 
 void json_string(const char *json, const char *key, char *buff) {
-  printf("json=%s, key=%s\n", json, key);
   char *k = strstr(json, key);
-  printf("k=%s\n", k);
   char *start = strchr(k+strlen(key), '\"')+1;
-  printf("start=%s\n", start);
   char *end = strchr(start, '\"');
-  printf("end=%s\n", end);
   memcpy(buff, start, end-start);
   buff[end-start] = '\0';
 }
